16e11a7182a297325bddb9776f3773836ac8591a
Gabriel Ojimah <ojimahgabriel2@gmail.com>
2020-05-09 21:08:08 +0100

    initial project version

---
+++ src/robots.js
@@ -0,0 +1,65 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Ojimah Gabriel',
+      username: 'Kismet',
+      email: 'Kismet@gmail.com'
+    },
+    {
+      id: 2,
+      name: 'Richard Monday',
+      username: 'Bobo',
+      email: 'bobo@gmail.com'
+    },
+    {
+      id: 3,
+      name: 'Timothy Raymond',
+      username: 'Gram',
+      email: 'gram@gmail.com'
+    },
+    {
+      id: 4,
+      name: 'Ochada Emmanuel',
+      username: 'Chadson',
+      email: 'chadson@brokeboiz.tv'
+    },
+    {
+      id: 5,
+      name: 'Babatunde Joseph',
+      username: 'Baba Tee',
+      email: 'babatee@gmail.com'
+    },
+    {
+      id: 6,
+      name: 'Oyedikachi Gideon',
+      username: 'Smart guy',
+      email: 'smartguy@gmail.com'
+    },
+    {
+      id: 7,
+      name: 'Ugbedeojo Samuel',
+      username: 'Samuel',
+      email: 'Samuel@shyguy.org'
+    },
+    {
+      id: 8,
+      name: 'Adbdulamid Isa',
+      username: 'Beard gang',
+      email: 'beardgang.gentle'
+    },
+    {
+      id: 9,
+      name: 'abdulganih Junior',
+      username: 'babby of the lodge',
+      email: 'babyofthelodge@gmail.com'
+    },
+    {
+      id: 10,
+      name: 'Damilare Abdulbasit',
+      username: 'Dreay',
+      email: 'Dreay@him.org'
+    }
+    
+  ];
+  
+    
